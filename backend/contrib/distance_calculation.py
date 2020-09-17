831bf0aa88ce62623bde2160f659338431d14a26
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-09-17 08:11:43 -0300

    Refactor distance function
    Use OO

--- backend/contrib/distance_calculation.py
+++ backend/contrib/distance_calculation.py
@@ -1,18 +1,39 @@
 from math import cos, asin, sqrt, pi
 
 
-def distance(lat1, lng1, lat2, lng2):
-    """https://en.wikipedia.org/wiki/Haversine_formula"""
+class GeoPoint:
+    def __init__(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
 
-    p = pi/180
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lng2-lng1)*p))/2
-    return 12742 * asin(sqrt(a)) #2*R*asin...
+    def __sub__(self, other):
+        """https://en.wikipedia.org/wiki/Haversine_formula"""
+
+        p_rad = pi / 180
+        angle = (
+            0.5
+            - cos((other.lat - self.lat) * p_rad) / 2
+            + cos(self.lat * p_rad)
+            * cos(other.lat * p_rad)
+            * (1 - cos((other.lng - self.lng) * p_rad))
+            / 2
+        )
+        earth_diameter = 12742
+
+        return earth_diameter * asin(sqrt(angle))
+
+
+def distance(lat1, lng1, lat2, lng2):
+    first_point = GeoPoint(lat1, lng1)
+    second_point = GeoPoint(lat2, lng2)
+
+    return first_point - second_point
 
 
 def calculate(markers):
-    lat1 = markers[0]['lat']
-    lat2 = markers[1]['lat']
-    lng1 = markers[0]['lng']
-    lng2 = markers[1]['lng']
+    lat1 = markers[0]["lat"]
+    lat2 = markers[1]["lat"]
+    lng1 = markers[0]["lng"]
+    lng2 = markers[1]["lng"]
 
-    return f'{distance(lat1, lng1, lat2, lng2):02.2f} km'
+    return f"{distance(lat1, lng1, lat2, lng2):02.2f} km"
