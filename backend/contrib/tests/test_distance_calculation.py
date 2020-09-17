7219c83dd42b2758ed487956dcb3fd6e56d6b370
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-09-17 07:42:03 -0300

    Remove pytest

--- backend/contrib/tests/test_distance_calculation.py
+++ backend/contrib/tests/test_distance_calculation.py
@@ -1,13 +1,11 @@
-import pytest
+import unittest
 from ..distance_calculation import distance
 
 
-@pytest.mark.django_db
-class TestDistanceCalculation:
-
+class TestDistanceCalculation(unittest.TestCase):
     def test_calculates_distance(self):
         buenos_aires = [-34.6120439, -58.4104192]
         salvador = [-12.9768177, -38.4569869]
         km = 3134.310335004333
 
-        assert distance(*buenos_aires, *salvador) == km
+        self.assertEqual( distance(*buenos_aires, *salvador), km)
